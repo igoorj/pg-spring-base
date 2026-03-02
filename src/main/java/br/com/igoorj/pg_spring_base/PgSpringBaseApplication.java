c5cdc753ef66d047e1a7dd16b64792c1eeadcef7
Igor Goncalves <goncalvesigor67@gmail.com>
2026-03-01 22:05:48 -0300

    first commit

---
+++ src/main/java/br/com/igoorj/pg_spring_base/PgSpringBaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.igoorj.pg_spring_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgSpringBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgSpringBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/igoorj/pg_spring_base/PgSpringBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igoorj.pg_spring_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgSpringBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
